b91899d2dbd5e5523b2cc70b400faebe858abe00
RohitOP1 <paulrohit804@gmail.com>
2025-03-03 11:23:55 +0530

    [Rohit] Add: Python script to write valid first name in User Rregistration Program

--- user.py
+++ user.py
@@ -1,1 +1,13 @@
-"Welcome to User Registration Program"
+import re
+
+def validate_name(name):
+    pattern = r'^[A-Z][a-zA-Z]{2,}$'
+    return bool(re.match(pattern, name))
+
+# Test
+first_name = input("Enter First Name: ")
+# last_name = input("Enter Last Name: ")
+
+print("First Name is valid:", validate_name(first_name))
+# print("Last Name is valid:", validate_name(last_name))
+
