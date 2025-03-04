81aecb48f317771f1755b5085fe9e9ad606db851
RohitOP1 <paulrohit804@gmail.com>
2025-03-04 11:57:30 +0530

    [Rohit] Add:python script to write condition that atlease one numeric digit should be there in password

--- user.py
+++ user.py
@@ -1,12 +1,12 @@
 import re
 
-def validate_password_uc6(password):
-    # Rule 2: Should have at least 1 uppercase letter
-    if not re.search(r'[A-Z]', password):
+def validate_password_uc7(password):
+    # Rule 3: Should have at least 1 numeric digit
+    if not re.search(r'\d', password):  # \d matches any digit (0-9)
         return False
     return True
 
 # Test
 password = input("Enter Password: ")
-print("Password is valid for UC6:", validate_password_uc6(password))
+print("Password is valid for UC7:", validate_password_uc7(password))
 
