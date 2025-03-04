3c0becf3e60b7d19d34122fa600ca334986eb1b4
RohitOP1 <paulrohit804@gmail.com>
2025-03-04 11:53:16 +0530

    [Rohit] Add:python script to write condition that atlease one uppercase should be there in password

--- user.py
+++ user.py
@@ -1,13 +1,12 @@
 import re
 
-def validate_password_uc5(password):
-    # Rule 1: Minimum 8 characters
-    if len(password) < 8:
+def validate_password_uc6(password):
+    # Rule 2: Should have at least 1 uppercase letter
+    if not re.search(r'[A-Z]', password):
         return False
     return True
 
 # Test
 password = input("Enter Password: ")
-print("Password is valid for UC5:", validate_password_uc5(password))
-
+print("Password is valid for UC6:", validate_password_uc6(password))
 
